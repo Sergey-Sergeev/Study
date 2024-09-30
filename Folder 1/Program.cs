f577834648184d4b87f89e15f4111e62c774b621
Sergey-Sergeev <112146784+Sergey-Sergeev@users.noreply.github.com>
2024-10-01 09:50:38 +1000

    Add some files
    add new files

---
+++ Folder 1/Program.cs
@@ -0,0 +1,12 @@
+using namespace System;
+
+namespace Program
+{
+    static class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Hell o world!");
+        }
+    }
+}
